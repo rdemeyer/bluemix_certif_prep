7824b8ca72b45457e151c3adf6e59496ffc29a24
romain.demeyer <romain.demeyer@gmail.com>
2017-01-23 11:35:30 -0500

    discover express and cloudant

--- server.js
+++ server.js
@@ -2,8 +2,13 @@
 
 const express = require('express');
 const Cloudant = require("cloudant");
+var bodyParser = require('body-parser');
 const port = process.env.PORT;
 const app = express();
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 
 if (process.env.VCAP_SERVICES) {
   var vcap = JSON.parse(process.env.VCAP_SERVICES);
@@ -18,6 +23,7 @@ if (process.env.VCAP_SERVICES) {
   }
 }
 
+//app.use(bodyParser);
 
 var cloudant = Cloudant(credentials, function(er, cloudant, reply) {
     if (er) throw er
@@ -29,14 +35,49 @@ cloudant.db.list(function(err, allDbs) {
     if (err) {
       return console.log('Failed to initialize Cloudant: ' + err.message);
     }
-
     console.log('All my databases: %s', allDbs.join(', '))
 });
+
+var db_certif = cloudant.db.use('db_certif_bluemix');
+
+
+
 
 app.get('/', function(req,res) {
   res.send('Hello');
   console.log("I send Hello once");
 });
+
+app.get('/getTeamBelgiumDoc', function(req,res) {
+  db_certif.get('ce419ef8ab82173a615b0b877a2ba7b5', { revs_info: true }, function(err, body) {
+    if (!err) {
+      console.log(body);
+      res.send(body.team_name); 
+    } else {
+      res.send('did not find it :( ');
+    }
+  });
+});
+
+app.get('/user/:id', function (req, res, next) {
+  res.end(req.params.id)
+});
+
+app.post('/postexample', function (req, res) {
+  //console.log(req.body.test);
+  //var test = req.json();
+  var tata = req.body.test;
+  
+  db_certif.insert({ crazy: true, test: tata }, function(err, body) {
+    if (!err)
+      console.log(body)
+  })
+  res.send("over");
+});
+
+
+
+
 
 app.listen(port);
 console.log("I listen on port "+ port);
